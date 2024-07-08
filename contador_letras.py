fcb0a892de1d4d0a51649a36b76c5ede84764edb
Paolosag08 <paolosag08@gmail.com>
2024-07-08 18:44:35 -0300

    realizamos un algoritmo que cuenta las letras de una palabra

---
+++ contador_letras.py
@@ -0,0 +1,11 @@
+# 1. Solicitar al usuario que ingrese una palabra
+
+palabra = input('Por favor, ingrese la palabra: ')
+
+# 2. Contariamos cuantas letras tiene la palabra
+
+longitud = len(palabra)
+
+# 3. Mostrar al usuario la cant de letras que tiene la palabra ingresada
+
+print(palabra,  'tiene',  longitud,  'letras.')
